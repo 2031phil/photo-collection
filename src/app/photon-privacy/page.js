77999c8a3ae17223c65c686f7e6f8359f523960d
2031phil <158180221+2031phil@users.noreply.github.com>
2026-02-19 20:15:32 +0100

    Added privacy policy for photon

---
+++ src/app/photon-privacy/page.js
@@ -0,0 +1,15 @@
+import './page.css';
+
+export default function photonPrivacy() {
+    return (
+        <main>
+            <div className='content-container'>
+                <div className='top-container'>
+                    <img id='photonLogo' src='/photon-logo.png' alt='Logo of Photon'></img>
+                    <h1>Photon Privacy Policy</h1>
+                </div>
+                <p>Photon does not collect personal data, browsing history, or identifiable information. Anonymous, aggregated usage statistics (such as total number of new tabs opened per day) may be collected to improve the product. No data is linked to individual users.</p>
+            </div>
+        </main>
+    )
+}
